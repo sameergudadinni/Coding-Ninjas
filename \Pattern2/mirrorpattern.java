14580614a21329556b6164447e0fe19aab665e1a
Sameer Gudadinni <117703668+sameergudadinni@users.noreply.github.com>
2022-12-13 09:19:45 +0530

    Create mirrorpattern.java

---
+++ \Pattern2/mirrorpattern.java
@@ -0,0 +1,77 @@
+// Code : Mirror Image Number Pattern
+// Send Feedback
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+
+
+
+
+// The dots represent spaces.
+
+
+// Input format :
+// Integer N (Total no. of rows)
+// Output format :
+// Pattern in N lines
+// Constraints
+// 0 <= N <= 50
+// Sample Input 1:
+// 3
+// Sample Output 1:
+//       1 
+//     12
+//   123
+// Sample Input 2:
+// 4
+// Sample Output 2:
+//       1 
+//     12
+//   123
+// 1234
+
+
+
+import java.util.Scanner;
+
+public class lecture6 {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+
+       Scanner sc=new Scanner(System.in);
+	   int n=sc.nextInt();
+	   int i=1;
+	   while(i<=n){
+		   int j=1;
+		   int start=1;
+		   while(j<=n-i){
+			   j++;
+			
+			   System.out.print(" ");
+		   }
+		   while(j<=n){
+			   System.out.print(start);
+			      start++;
+				  j++;
+
+		   }
+		   i++;
+		   System.out.println();
+	   }
+		
+	}
+
+}
+
+
+     
+        
+     
+     
+     }
+}
